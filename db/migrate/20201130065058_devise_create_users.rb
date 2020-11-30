540e8b36fda196388a5d51e6ff7e8cfba9e83061
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-11-30 16:57:25 +0900

    カラムの追加

--- db/migrate/20201130065058_devise_create_users.rb
+++ db/migrate/20201130065058_devise_create_users.rb
@@ -4,9 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+      t.string  :first_name,         null: false
+      t.string  :last_name,          null: false
+      t.string  :first_name_kana,    null: false
+      t.string  :last_name_kana,     null: false 
+      t.string  :email,              null: false, default: "", unique: true
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :postal_code,        null: false
+      t.integer :state_id,           null: false
+      t.string  :city,               null: false
+      t.string  :address_number,     null: false
+      t.string  :building_name
+      t.string  :phone_number,       null: false
+      t.date  :birth_day,          null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,19 @@
 ActiveRecord::Schema.define(version: 2020_11_30_065058) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "postal_code", null: false
+    t.integer "state_id", null: false
+    t.string "city", null: false
+    t.string "address_number", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
