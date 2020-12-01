2aab23524f572e2e7f67e2fa3f1ba4a899e58728
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-01 14:11:39 +0900

    軽微な修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.includes(:user).order('created_at DESC')
+    @articles = Article.includes(:user).order('created_at DESC').limit(4)
   end
 end
