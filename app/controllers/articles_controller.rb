55edd787b630dcfc6ee305f8958fd270427819f5
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-01 15:36:38 +0900

    コントローラーの修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
+  before_action :search_article, only: [:index, :search]
   def index
     @articles = Article.includes(:user).order('created_at DESC').limit(4)
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def search
+  end
+
+  private
+
+  def search_article
+    @a = Article.ransack(params[:q])
+    @results = @a.result
   end
 end
