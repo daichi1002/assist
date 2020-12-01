0cb0f85a320bd4af39e2fb0545b993fa80d84aec
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-01 12:28:01 +0900

    バリデーションの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  belongs_to :area
+  belongs_to :genre
+  belongs_to :type
+  has_one_attached :image
+
+  validates :image, :title, :genre_id, :type_id, :area_id, :maximum_price, :start_date, :end_date, :information, :url, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_094422) do
+ActiveRecord::Schema.define(version: 2020_12_01_032421) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "genre_id", null: false
+    t.integer "type_id", null: false
+    t.integer "area_id", null: false
+    t.string "maximum_price", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.text "information", null: false
+    t.string "url", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -41,5 +57,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_094422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "sns_credentials", "users"
 end
