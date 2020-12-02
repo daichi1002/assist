21af142c2c75be394a257370b3065b61970c8ffa
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-02 14:08:52 +0900

    非同期いいねの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
+  before_action :article_params
   def create
     Like.create(user_id: current_user.id, article_id: params[:id])
-    redirect_to articles_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, article_id: params[:id]).destroy
-    redirect_to articles_path
   end
 
+  def article_params
+    @article = Article.find(params[:id])
+  end
 end
