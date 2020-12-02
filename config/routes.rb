a913404331ff9564cd8fb90456aac6646ea3bba7
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-02 14:40:35 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
+  resources :users, only: [:show]
   
   root to: 'articles#index'
   get 'articles/search'
