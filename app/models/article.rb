b31825a2fb64e3ccf4bebd62b5a9ad0d773b8741
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-07 16:42:57 +0900

    モデルの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
   has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :image, :title, :genre_id, :type_id, :area_id, :maximum_price, :start_date, :end_date, :information, :url, presence: true
 end
