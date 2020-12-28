2efbbd8ea7aaa0438a25774430b484fba17cbee4
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-28 10:41:01 +0900

    companies/likesコントローラーの編集

--- app/controllers/companies/likes_controller.rb
+++ app/controllers/companies/likes_controller.rb
@@ -1,2 +1,14 @@
 class Companies::LikesController < ApplicationController
+  before_action :article_params
+  def create
+    Like.create(company_id: current_company.id, article_id: params[:id])
+  end
+
+  def destroy
+    Like.find_by(company_id: current_company.id, article_id: params[:id]).destroy
+  end
+
+  def article_params
+    @article = Article.find(params[:id])
+  end
 end
