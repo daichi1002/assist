6e59de3480cefbd92ff269da921f5a4788ef825a
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-12-28 10:40:43 +0900

    companies/commentsコントローラーの編集

--- app/controllers/companies/comments_controller.rb
+++ app/controllers/companies/comments_controller.rb
@@ -1,2 +1,22 @@
 class Companies::CommentsController < ApplicationController
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new
+    @comments = @article.comments.includes(:user, :company)
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(company_id: current_company.id, article_id: params[:article_id])
+  end
 end
