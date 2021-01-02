62d528dd7ba116eaa905cb5c9a316fa5c050242a
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-02 15:41:30 +0900

    カラムの追加

--- db/migrate/20210102060823_create_matchings.rb
+++ db/migrate/20210102060823_create_matchings.rb
@@ -1,7 +1,14 @@
 class CreateMatchings < ActiveRecord::Migration[6.0]
   def change
     create_table :matchings do |t|
-
+      t.text :title, null: false
+      t.text :detail, null: false
+      t.integer :need_id, null: false
+      t.date :end_date
+      t.string :contact_information, null: false
+      t.string :url
+      t.references :user, foreign_key: true
+      t.references :company, foreign_key: true
       t.timestamps
     end
   end
