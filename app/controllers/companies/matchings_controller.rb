97a5aea2523b864c317821b1601a8716a8e17764
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-03 14:19:20 +0900

    matching_controllerの編集

--- app/controllers/companies/matchings_controller.rb
+++ app/controllers/companies/matchings_controller.rb
@@ -4,4 +4,17 @@ class Companies::MatchingsController < ApplicationController
   def index
     @matchings = Matching.all
   end
+
+  def new
+    @marching = Matching.new
+  end
+
+  def create
+    @matching = Matching.new(matching_params)
+    if @matching.save
+      redirect_to companies_matchings_path
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -4,4 +4,17 @@ class MatchingsController < ApplicationController
   def index
     @matchings = Matching.all
   end
+
+  def new
+    @marching = Matching.new
+  end
+
+  def create
+    @matching = Matching.new(matching_params)
+    if @matching.save
+      redirect_to matchings_path
+    else
+      render :new
+    end
+  end
 end
