4e98a186a0b41af4991313d0e415c816c728fef1
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-05 15:10:25 +0900

    matchings_controllerの編集

--- app/controllers/companies/matchings_controller.rb
+++ app/controllers/companies/matchings_controller.rb
@@ -1,5 +1,7 @@
 class Companies::MatchingsController < ApplicationController
   before_action :authenticate_company!
+  before_action :search_matching, only: [:index, :search]
+
 
   def index
     @matchings = Matching.all
@@ -23,9 +25,17 @@ class Companies::MatchingsController < ApplicationController
     @need = Need.find_by_id @matching.need_id
   end
 
+  def search
+    @results = @p.result(distinct: true).page(params[:page]).per(8).order('created_at DESC')
+  end
+
   private
 
   def matching_params
     params.permit(:title, :need_id, :detail, :end_date, :contact_information, :url).merge(company_id: current_company.id)
+  end
+
+  def search_matching
+    @p = Matching.ransack(params[:q])
   end
 end
--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -1,5 +1,6 @@
 class MatchingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :search_matching, only: [:index, :search]
 
   def index
     @matchings = Matching.all
@@ -23,9 +24,17 @@ class MatchingsController < ApplicationController
     @need = Need.find_by_id @matching.need_id
   end
 
+  def search
+    @results = @p.result(distinct: true).page(params[:page]).per(8).order('created_at DESC')
+  end
+
   private
 
   def matching_params
     params.permit(:title, :need_id, :detail, :end_date, :contact_information, :url).merge(user_id: current_user.id)
+  end
+
+  def search_matching
+    @p = Matching.ransack(params[:q])
   end
 end
