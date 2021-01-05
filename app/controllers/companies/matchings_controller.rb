252f2d5f89652ad88685ba6a0367fee6c04e0a2f
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-05 16:34:20 +0900

    matching_controllerの編集

--- app/controllers/companies/matchings_controller.rb
+++ app/controllers/companies/matchings_controller.rb
@@ -1,5 +1,6 @@
 class Companies::MatchingsController < ApplicationController
   before_action :authenticate_company!
+  before_action :set_matching, only: [:show, :edit, :update]
   before_action :search_matching, only: [:index, :search]
 
 
@@ -21,8 +22,18 @@ class Companies::MatchingsController < ApplicationController
   end
 
   def show
-    @matching = Matching.find(params[:id])
     @need = Need.find_by_id @matching.need_id
+  end
+
+  def edit
+  end
+
+  def update
+    if @matching.update(matching_params)
+      redirect_to companies_matching_path(matching.id)
+    else
+      render :edit
+    end
   end
 
   def search
@@ -33,6 +44,10 @@ class Companies::MatchingsController < ApplicationController
 
   def matching_params
     params.permit(:title, :need_id, :detail, :end_date, :contact_information, :url).merge(company_id: current_company.id)
+  end
+
+  def set_matching
+    @matching = Matching.find(params[:id])
   end
 
   def search_matching
