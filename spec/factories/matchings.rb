b200c4c4452cdcb10464b7aa5cd8b34f316bfe5c
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-12 16:52:19 +0900

    Update matchings.rb

--- spec/factories/matchings.rb
+++ spec/factories/matchings.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :matching do
     title { Faker::Lorem.sentence }
     need_id { 1 }
+    area_id { 1 }
     detail { Faker::Lorem.sentence }
     end_date { Faker::Date.between(from: '2022-12-01', to: '2023-12-01') }
     contact_information { Faker::Lorem.sentence }
