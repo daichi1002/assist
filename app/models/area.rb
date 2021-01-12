7d1bb9ae8a6f9c47f281c4db5ad9c1b8d27eb893
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-12 11:57:56 +0900

    アソシエーションの設定

--- app/models/area.rb
+++ app/models/area.rb
@@ -52,4 +52,5 @@ class Area < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :articles
+  has_many :matchings
 end
--- app/models/matching.rb
+++ app/models/matching.rb
@@ -1,6 +1,8 @@
 class Matching < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user, optional: true
   belongs_to :company, optional: true
+  belongs_to :area
 
   validates :title, :need_id, :detail, :contact_information, presence: true
 end
