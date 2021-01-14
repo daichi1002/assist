7fbc769e757a5e5e0e020cf6c7bd1e72ffc41f79
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2021-01-14 11:11:57 +0900

    Update comments_spec.rb

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'コメント投稿', type: :system do
   before do
     @user = FactoryBot.create(:user)
+    @company = FactoryBot.create(:company)
     @article = FactoryBot.create(:article)
     @comment = FactoryBot.build(:comment)
   end
@@ -24,6 +25,24 @@ RSpec.describe 'コメント投稿', type: :system do
         find('input[name="commit"]').click
       end.to change { Comment.count }.by(1)
       expect(current_path).to eq article_comments_path(@article)
+    end
+
+    it 'ログインした法人会員はコメントを投稿できる' do
+      visit root_path
+      expect(page).to have_content('法人会員はこちら')
+      visit new_company_session_path
+      fill_in 'メールアドレス', with: @company.email
+      fill_in 'パスワード', with: @company.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq companies_articles_path
+      visit companies_article_path(@article)
+      expect(page).to have_selector('.fa-comments')
+      visit new_companies_article_comment_path(@article)
+      fill_in '内容', with: @comment.text
+      expect do
+        find('input[name="commit"]').click
+      end.to change { Comment.count }.by(1)
+      expect(current_path).to eq companies_article_comments_path(@article)
     end
   end
 
